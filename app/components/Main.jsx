e2038a918bd9e03f7caf685f9281f24895ebc084
Oscar Barillas <oscarbarillas93@gmail.com>
2016-06-24 22:58:01 -0700

    Added Countdown Form + Testing for it using spies

--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -4,9 +4,8 @@ var Main = (props) => {
   return (
     <div>
       <Nav />
-      <div>
-        <div>
-          <p>Main.jsx Rendered</p>
+      <div className = "row">
+        <div className = "column small-centered medium-6 large-6 ">
           {props.children}
         </div>
       </div>
